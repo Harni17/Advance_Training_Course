48a411d979b77de0034359ddd1cdd27f40700a1e
Harni17 <97238928+Harni17@users.noreply.github.com>
2022-03-17 22:41:32 +0530

    Add files via upload

---
+++ Core Java/Problem Statement 1/Problem_Statement1_1/Even_Number.java
@@ -0,0 +1,21 @@
+package HarniK.Problem_Statement1_1;
+
+public class Even_Number {
+
+public static void main(String[] args) {
+
+		int n = scan.nextInt();
+
+		System.out.print("Even Numbers from 1 to "+n+" are:\n ");
+
+		for (int i = 1; i <= n; i++) {
+                       if (i % 2 == 0) {
+                         System.out.print(i + " ");
+
+		   }
+
+		}
+
+	}
+
+		
