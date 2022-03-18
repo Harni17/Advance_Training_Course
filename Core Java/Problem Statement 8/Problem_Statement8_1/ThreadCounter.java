432fa7c18a6569f84f48abead30f177641456d8e
Harni17 <97238928+Harni17@users.noreply.github.com>
2022-03-18 10:27:06 +0530

    Add files via upload

---
+++ Core Java/Problem Statement 8/Problem_Statement8_1/ThreadCounter.java
@@ -0,0 +1,12 @@
+package HarniK.Problem_Statement8_1;
+
+public class ThreadCounter {
+        public static void main(String args[]){
+
+	Counter counter = new Counter(25);
+
+	counter.run();
+
+	}
+
+	}
