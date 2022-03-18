3167ab6ff2aa0ff1d665002a99018c75060efe53
Harni17 <97238928+Harni17@users.noreply.github.com>
2022-03-18 10:07:22 +0530

    Add files via upload

---
+++ Core Java/Problem Statement 7/Problem_Statement7_2/Demo.java
@@ -0,0 +1,7 @@
+package HarniK.Problem_Statement7_2;
+
+class NameNotValidException extends Exception{
+     public String validname(){
+          return ("Name is not Valid..Please ReEnter the Name");
+     }
+}
